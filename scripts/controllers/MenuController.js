25b2d2735cb17298091c13c296ed7dd433e8787b
Miriam Merchante <miriam.merchante@babel.es>
2016-03-21 11:17:11 +0100

    Empezando con controllers

---
+++ scripts/controllers/MenuController.js
@@ -0,0 +1,23 @@
+angular.module("moviedb").controller("MenuController", 	
+	["$scope", function($scope) {
+
+		//Scope init
+		$scope.model = {
+			selectedItem:"movies"
+		};
+
+		//Scope methods
+		$scope.setSelectedItem = function(item){
+			$scope.model.selectedItem = item;
+		}
+
+		$scope.getClassForItem = function(item){
+			if($scope.model.selectedItem == item){
+				return "active";
+			} else {
+				return "";
+			}
+		}
+
+	}]
+);
