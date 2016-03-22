6b0729b7ca4ad5e21ee26e99dc1d41f9e59e809a
Miriam Merchante <miriam.merchante@babel.es>
2016-03-22 13:48:53 +0100

    Cargar películas usando servicio http

--- scripts/services/MovieService.js
+++ scripts/services/MovieService.js
@@ -1,25 +1,6 @@
-angular.module("moviedb").service("MovieService", ["$q", "$timeout", function($q, $timeout) {
-    var movies = [];
-
+angular.module("moviedb").service("MovieService", ["$http", function($http) {
     this.getMovies = function() {
-        // Creamos el objeto diferido
-
-        var deferred = $q.defer();
-
-        // Asincronía
-        $timeout(function() {
-            if (Math.round(Math.random() * 10) % 2 == 0) {
-                // Resolvemos la promesa
-                deferred.resolve(movies);
-            } else {
-                // Rechazamos la promesa
-                deferred.reject({ error: "Forbidden" });
-            }
-
-        }, 500);
-
-        //Devolvemos la promesa del objeto diferido
-        return deferred.promise;
+        return $http.get('api/movies');
 
     }
 }]);
