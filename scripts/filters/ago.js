5ebbc1206a3e74e1f390572b2608a3c2f8cee7cd
Miriam Merchante <miriam.merchante@babel.es>
2016-03-23 10:10:56 +0100

    Filtros ago y schoolrating

---
+++ scripts/filters/ago.js
@@ -0,0 +1,10 @@
+angular.module("moviedb").filter("ago", [function() {
+        return function(text) {
+        	return moment(text).fromNow();
+
+        };
+
+    }]
+
+
+);
---
+++ scripts/filters/schoolrating.js
@@ -0,0 +1,20 @@
+angular.module("moviedb").filter("schoolrating", [function() {
+    return function(rating, mode) {
+        if (rating <= 10 && rating >= 9) {
+            return (mode == "US") ? "A" : "SB";
+
+        } else if (rating < 9 && 7 <= rating) {
+            return (mode == "US") ? "B" : "NT";
+
+        } else if (rating < 7 && 6 <= rating) {
+            return (mode == "US") ? "C" : "BN";
+
+        } else if (rating < 6 && 5 <= rating) {
+            return (mode == "US") ? "D" : "SF";
+
+        } else if (rating < 5) {
+            return (mode == "US") ? "E" : "IN";
+
+        }
+    }
+}]);
