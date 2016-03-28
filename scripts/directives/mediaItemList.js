d0a5beb255e6b6bfb4a1faefe43a37b96b07a8b0
Miriam Merchante <miriam.merchante@babel.es>
2016-03-28 12:59:29 +0200

    Paso de métodos de la directiva esta vez con & dentro del scope

--- scripts/directives/mediaItemList.js
+++ scripts/directives/mediaItemList.js
@@ -6,7 +6,7 @@ angular.module("moviedb").directive('mediaItemList', function() {
         // terminal: true,
          scope: {
          	model:"=items",
-         	getDetailUrl:"="
+         	getDetailUrl:"&"
          }, // {} = isolate, true = child, false/undefined = no change
         // controller: function($scope, $element, $attrs, $transclude) {},
         // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
